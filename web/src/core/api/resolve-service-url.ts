b1bb58594239db25deb4c3135f8b4e862c05af0d
martin <18100658783@163.com>
2025-06-25 14:21:32 +0800

    use fixed url of back end

--- web/src/core/api/resolve-service-url.ts
+++ web/src/core/api/resolve-service-url.ts
@@ -4,7 +4,7 @@
 import { env } from "~/env";
 
 export function resolveServiceURL(path: string) {
-  let BASE_URL = env.NEXT_PUBLIC_API_URL ?? "http://localhost:8000/api/";
+  let BASE_URL = "http://192.168.31.194:8000/api/";
   if (!BASE_URL.endsWith("/")) {
     BASE_URL += "/";
   }
