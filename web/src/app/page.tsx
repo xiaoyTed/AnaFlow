49130be632cc37b8510079dbf440bccd8a6ab2e1
martin <18100658783@163.com>
2025-06-27 14:31:17 +0800

    remove main page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,36 +1,3 @@
-// Copyright (c) 2025 Bytedance Ltd. and/or its affiliates
-// SPDX-License-Identifier: MIT
+import HomePage from "./chat/page";
 
-import { useMemo } from "react";
-
-import { SiteHeader } from "./chat/components/site-header";
-import { Jumbotron } from "./landing/components/jumbotron";
-import { Ray } from "./landing/components/ray";
-import { MultiAgentSection } from "./landing/sections/multi-agent-section";
-
-export default function HomePage() {
-  return (
-    <div className="flex flex-col items-center">
-      <SiteHeader />
-      <main className="container flex flex-col items-center justify-center gap-56">
-        <Jumbotron />
-        <MultiAgentSection />
-      </main>
-      <Footer />
-      <Ray />
-    </div>
-  );
-}
-
-function Footer() {
-  const year = useMemo(() => new Date().getFullYear(), []);
-  return (
-    <footer className="container mt-32 flex flex-col items-center justify-center">
-      <hr className="from-border/0 via-border/70 to-border/0 m-0 h-px w-full border-none bg-gradient-to-r" />
-      <div className="text-muted-foreground container mb-8 flex flex-col items-center justify-center text-xs">
-        <p>Licensed under MIT License</p>
-        <p>&copy; {year} AnaFlow</p>
-      </div>
-    </footer>
-  );
-}
+export default HomePage;
